fac86b0a0d63b2162cfc0bd1eae9ea48d39e1d60
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2022-03-06 14:38:08 +0100

    kanji (#12) : Ajout du formulaire de rechercher. Ajout de la page des kanji et pagination

--- src/Controller/IdeogrammeController.php
+++ src/Controller/IdeogrammeController.php
@@ -2,15 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Data\FilterData;
+use App\Form\FilterType;
+use App\Entity\Ideogramme;
+use App\Handler\IdeogrammeReadHandler;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IdeogrammeController extends AbstractController
 {
+    public function __construct(private IdeogrammeReadHandler $ideogrammeReadHandler)
+    {}
+
     #[Route('/histoire', name: 'ideogramme')]
     public function index(): Response
     {
         return $this->render('ideogramme/index.html.twig');
+    }
+
+    #[Route('/les-kanji', name: 'ideogramme.list')]
+    public function listKanji(Request $request): Response
+    {
+        $data = new FilterData();
+        $data->page = $request->query->getInt('page', 1);
+
+        $form = $this->createForm(FilterType::class, $data);
+        $form->handleRequest($request);
+
+        $ideogrammes = $this->ideogrammeReadHandler->getSearch($data);
+
+        return $this->renderForm('ideogramme/list-kanji.html.twig', [
+            'ideogrammes' => $ideogrammes,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/kanji/{id}', name: 'ideogramme.show', requirements: ['id' => '\d+'])]
+    public function showKanji(Ideogramme $ideogramme): Response
+    {
+        return $this->render('ideogramme/show-kanji.html.twig', [
+            'ideogramme' => $ideogramme
+        ]);
     }
 }
--- src/Handler/IdeogrammeReadHandler.php
+++ src/Handler/IdeogrammeReadHandler.php
@@ -6,8 +6,10 @@
 
 namespace App\Handler;
 
+use App\Data\FilterData;
 use App\Entity\Ideogramme;
 use App\Repository\IdeogrammeRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
 
 class IdeogrammeReadHandler
 {
@@ -24,5 +26,20 @@ class IdeogrammeReadHandler
     public function getLastKanji(int $number): array
     {
         return $this->repository->findBy([], [], $number);
+    }
+    
+    /**
+     * getAll
+     *
+     * @return array<Ideogramme>
+     */
+    public function getAll(): array
+    {
+        return $this->repository->findAll();
+    }
+
+    public function getSearch(FilterData $data): PaginationInterface
+    {
+        return $this->repository->findSearch($data);
     }
 }
